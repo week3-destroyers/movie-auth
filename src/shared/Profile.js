efba1fe904f6a8f5f7aff70758677c9fdf1bd8de
sepuckett86 <sepuckett86@gmail.com>
2019-06-03 14:53:21 -0700

    finish tests for Profile component

--- src/shared/Profile.js
+++ src/shared/Profile.js
@@ -5,10 +5,14 @@ import Component from '../Component.js';
 class Profile extends Component {
     renderTemplate() {
         const user = this.props.user;
+        if(!user) {
+            return `<div></div>`;
+        }
+        const avatar = user.photoURL || './assets/default-image.jpg';
 
         return /*html*/ `
             <div class="profile">
-                <img id="profile-image" src="${user.photoURL}">
+                <img id="profile-image" src="${avatar}">
                 <div>
                     <span>${user.displayName}</span>
                     <button>Sign Out</button>
--- test/Profile.test.js
+++ test/Profile.test.js
@@ -29,3 +29,46 @@ test('make sure there is a user', assert => {
     //assert
     assert.htmlEqual(actual, expected);
 });
+
+test('renders with default avatar when no photo URL', assert => {
+    //arrange
+    const user = {
+        displayName: 'Static Name',
+        photoURL: null
+    };
+
+    const profile = new Profile({ user });
+
+    const expected = /*html*/ `
+        <div class="profile">
+            <img id="profile-image" src="./assets/default-image.jpg">
+            <div>
+                <span>Static Name</span>
+                <button>Sign Out</button>
+            </div>
+        </div>
+    `;
+
+    //act
+    const actual = profile.renderTemplate();
+
+    //assert
+    assert.htmlEqual(actual, expected);
+});
+
+test('renders empty div when no user', assert => {
+    //arrange
+    const user = null;
+
+    const profile = new Profile({ user });
+
+    const expected = /*html*/ `
+        <div></div>
+    `;
+
+    //act
+    const actual = profile.renderTemplate();
+
+    //assert
+    assert.htmlEqual(actual, expected);
+});
